71f26183deffab50aeb2a690d22b56ec40e09de2
awei <274635143@qq.com>
2019-12-07 16:40:05 +0800

    验证码改为极验

--- src/api/login.js
+++ src/api/login.js
@@ -13,6 +13,12 @@ const info = params => fetch('/blog/manage/login/info', {params: params, type: '
  */
 const login = data => fetch('/blog/manage/login/login', {data: data, type: 'POST'});
 /**
+ * Gt 登陆
+ * @param data
+ * @returns {Promise<unknown>}
+ */
+const gt_login = data => fetch('/blog/manage/login/gt_login', {data: data, type: 'POST'});
+/**
  * lw 注册
  * @param name 昵称
  * @param user 账号
@@ -24,5 +30,6 @@ const register = data => fetch('/blog/manage/login/register', {data: data, type:
 export {
   info,
   login,
+  gt_login,
   register,
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,18 +2,25 @@
   <div class="bg">
     <div class="mask"></div>
     <div class="login">
-      <div class="title">登&emsp;录</div>
-      <el-form status-icon label-position="top" :model="dat" ref="login" @submit.native.prevent>
-        <el-form-item prop="user" label="账号" :rules="[{ required: true, message: ' '}]">
+      <div class="title">博客后台管理系统</div>
+      <el-form status-icon label-position="top" :rules="rules" :model="dat" ref="login" @submit.native.prevent>
+        <el-form-item prop="user" label="账号">
           <el-input v-model="dat.user" style="width: 220px" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item prop="password" label="密码" :rules="[{ required: true, message: ' '}]">
+        <el-form-item prop="password" label="密码">
           <el-input v-model="dat.password" style="width: 220px" type="password" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item prop="code" label="验证码" :rules="[{ required: true, message: ' '}]">
-          <el-input class="code_input" v-model="dat.code" style="width: 114px" autocomplete="off"></el-input>
-          <div class="code" v-html="svg" @click="getCode"></div>
+<!--        <el-form-item prop="code" label="验证码" :rules="[{ required: true, message: ' '}]">-->
+<!--          <el-input class="code_input" v-model="dat.code" style="width: 114px" autocomplete="off"></el-input>-->
+<!--          <div class="code" v-html="svg" @click="getCode"></div>-->
+<!--        </el-form-item>-->
+
+        <el-form-item prop="gtCode" label="验证">
+          <div id="captcha">
+            <p class="code_load" v-if="codeLoadSta">正在加载验证码......</p>
+          </div>
         </el-form-item>
+
         <el-form-item>
           <!--<el-button type="primary" @click="submitForm()">登录</el-button>-->
           <el-button type="primary" @click="submitForm()" native-type="submit">登&emsp;录</el-button>
@@ -26,24 +33,40 @@
 
 <script>
   import MD5 from 'js-md5'
-  import {login} from '@/api/login'
-  import {code} from '@/api/verification'
+  import {gt_login} from '@/api/login'
+  import {code, gtCode} from '@/api/verification'
+  import '@/assets/js/gt'
 
   export default {
 
     data() {
+      const gtCodeRule = (rule, value, callback) => {
+        if (document.querySelector("input[name='geetest_challenge']").value && document.querySelector("input[name='geetest_validate']").value) {
+          callback();
+        } else {
+          callback(new Error(' '));
+        }
+      };
       return {
+        rules: {
+          user: [{ required: true, message: ' '}],
+          password: [{ required: true, message: ' '}],
+          // code: [{ required: true, message: ' '}],
+          gtCode: [{validator: gtCodeRule, trigger: ['blur', 'change']}],
+        },
         svg: '',
         dat: {
           user: '',
           password: '',
           code: '',
           key: '',
-        }
+        },
+        codeLoadSta: true
       }
     },
     created() {
-      this.getCode();
+      // this.getCode();
+      this.getGtCode();
     },
     methods: {
       getCode() {
@@ -56,16 +79,51 @@
           }
         })
       },
+      handler(captchaObj) {
+        console.log(captchaObj);
+        let _this = this;
+        captchaObj.appendTo('#captcha'); // 同时插入三个input表单时，geetest_challenge, geetest_validate, geetest_seccode
+        captchaObj.onReady(function () {
+          // $("#wait").hide();
+          _this.codeLoadSta = false;
+        });
+      },
+      getGtCode() {
+        gtCode({t: (new Date()).getTime()}).then(res => {
+          let data = res.data;
+          // 调用 initGeetest 进行初始化
+          // 参数1：配置参数
+          // 参数2：回调，回调的第一个参数验证码对象，之后可以使用它调用相应的接口
+          initGeetest({
+            // 以下 4 个配置参数为必须，不能缺少
+            gt: data.gt,
+            challenge: data.challenge,
+            offline: !data.success, // 表示用户后台检测极验服务器是否宕机
+            new_captcha: data.new_captcha, // 用于宕机时表示是新验证码的宕机
+
+            product: 'float', // 产品形式，包括：float，popup
+            width: '220px'
+            // 更多配置参数说明请参见：http://docs.geetest.com/install/client/web-front/
+          }, this.handler);
+        })
+      },
       submitForm() {
         this.$refs.login.validate((valid) => {
           if (valid) {
+            // let dat = {
+            //   user: this.dat.user,
+            //   password: MD5(this.dat.password),
+            //   code: this.dat.code,
+            //   key: this.dat.key,
+            // };
             let dat = {
               user: this.dat.user,
               password: MD5(this.dat.password),
-              code: this.dat.code,
-              key: this.dat.key,
+              geetest_challenge: document.querySelector("input[name='geetest_challenge']").value,
+              geetest_validate: document.querySelector("input[name='geetest_validate']").value,
+              geetest_seccode: document.querySelector("input[name='geetest_seccode']").value,
             };
-            login(dat).then(res => {
+            gt_login(dat).then(res => {
               if (res.code === 200) {
                 this.$message.success('登录成功');
                 this.$router.replace({path: '/admin'});
@@ -75,7 +133,8 @@
                 sessionStorage.setItem('user', res.data.user);
                 sessionStorage.setItem('id', res.data.id);
               } else if (res.code === 1) {
-                this.getCode();
+                // this.getCode();
+                this.$message.error(res.message);
               } else {
                 this.$message.error(res.message);
               }
@@ -95,7 +154,7 @@
   /deep/ .el-input__inner {
     border: 1px solid #333;
     background-color: transparent;
-    border-radius: 0;
+    border-radius: 2px;
     box-shadow: 4px 4px 0 rgba(51, 51, 51, .5);
   }
 
@@ -129,8 +188,8 @@
     box-shadow: 4px 4px 0 rgba(255, 255, 255, 0.4);
 
     .title {
-      color: #666666;
-      font-size: 22px;
+      color: #333333;
+      font-size: 20px;
       font-weight: 400;
       margin: 20px 0 30px 0;
       width: 220px;
@@ -139,7 +198,7 @@
 
     button {
       width: 220px;
-      border-radius: 0;
+      border-radius: 2px;
       box-shadow: 4px 4px 0 rgba(64, 158, 255, .5);
     }
 
@@ -154,19 +213,31 @@
 
     .code {
       vertical-align: middle;
-      background-color: #333;
+      background-color: rgba(255, 255, 255, .5);
       display: inline-block;
       width: 100px;
       height: 40px;
       border-radius: 0;
       box-sizing: border-box;
-      border: 0;
       cursor: pointer;
       box-shadow: 4px 4px 0 rgba(51, 51, 51, .5);
+      border: 1px solid #333;
 
       svg {
         width: 100%;
         height: 100%;
+      }
+    }
+
+    .code_load {
+      margin: 0;
+      color: #999;
+    }
+    /deep/ .geetest_holder {
+      min-width: 220px;
+      .geetest_radar_btn {
+        border-color: #FFFFFF;
+        box-shadow: 4px 4px 0 rgba(51, 51, 51, 0.5);
       }
     }
   }
