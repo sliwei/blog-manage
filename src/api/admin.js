1f201052575c3a259473ec9df363d403cb6d3f4a
awei <274635143@qq.com>
2020-01-02 11:31:04 +0800

    test1 1

--- src/api/admin.js
+++ src/api/admin.js
@@ -3,6 +3,16 @@ import fetch from '@/assets/js/axios';
 /**
  * lw 获取服务器信息
  */
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
 const sys = () => fetch('/blog/manage/admin/sys', {type: 'GET'});
 
 export {
